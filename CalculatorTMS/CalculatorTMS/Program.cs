e3f5330dee50f136e55df8ff68263a40e4db9622
EgorRed <egor.red02@gmail.com>
2023-10-09 18:57:51 +0300

    add calculator

---
+++ CalculatorTMS/CalculatorTMS/Program.cs
@@ -0,0 +1,14 @@
+﻿using CalculatorTMS;
+
+
+
+static double Calculate(in string expression)
+{
+    Tokenizer tokenizer = new Tokenizer();
+    ShuntingYardAlgorithm shuntingYardAlgorithm = new ShuntingYardAlgorithm();
+    PostfixNotationCalculator postfixNotationCalculator = new PostfixNotationCalculator();
+    return postfixNotationCalculator.Calculate(shuntingYardAlgorithm.Apply(tokenizer.Parse(expression))).Value;
+}
+
+Console.WriteLine(Calculate("2 + 8 * 23 * 5 / 2 + (2 * 5)"));
+
