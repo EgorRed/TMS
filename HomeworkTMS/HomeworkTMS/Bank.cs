7e2b49283d8abe68e108c1423ae091fc33b28e39
EgorRed <egor.red02@gmail.com>
2023-10-12 19:11:45 +0300

    add homework

---
+++ HomeworkTMS/HomeworkTMS/Bank.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkTMS
+{
+    public struct MonthData
+    {
+        public DateTime firstDayOfMonth { get; set; }
+        public DateTime lastDayOfMonth { get; set; }
+        public double percent { get; set; }
+        public decimal depositAmount { get; set; }
+        public decimal revenueForTheCurrentMonth { get; set; }
+    }
+
+    public class Bank
+    {
+        public List<MonthData> calculateContribution(in int countOfMonth, in double percent, in decimal depositAmount)
+        {
+            DateTime _dateTime = DateTime.Now;
+            _dateTime = _dateTime.AddDays(1);
+            decimal _depositAmount = depositAmount;
+            var _daysEndOfTheMonth = DaysEndOfTheMonth(_dateTime);
+
+            for (int i = 0; i <= countOfMonth; i++)
+            {
+                MonthData monthData = new();
+                monthData.firstDayOfMonth = _dateTime;
+                monthData.percent = percent;
+                monthData.depositAmount = _depositAmount;
+                if(i == 0)
+                {
+                    
+                    monthData.revenueForTheCurrentMonth = CalculateThePercentage
+                        (_depositAmount, (decimal)monthData.percent, _daysEndOfTheMonth);
+
+                    _depositAmount += monthData.revenueForTheCurrentMonth;
+
+                    monthData.lastDayOfMonth = _dateTime.AddDays(_daysEndOfTheMonth+1);
+                    _daysEndOfTheMonth = DateTime.DaysInMonth(_dateTime.Year, _dateTime.Month);
+                    _dateTime = monthData.lastDayOfMonth;
+                    ArrayMonthData.Add(monthData);
+
+                }
+                else if (i == countOfMonth)
+                {
+                    monthData.revenueForTheCurrentMonth = CalculateThePercentage
+                        (_depositAmount, (decimal)monthData.percent, ArrayMonthData.First().firstDayOfMonth.Day);
+
+                    _depositAmount += monthData.revenueForTheCurrentMonth;
+
+                    monthData.lastDayOfMonth = _dateTime.AddDays(ArrayMonthData.First().firstDayOfMonth.Day-2);
+                    ArrayMonthData.Add(monthData);
+
+                }
+                else
+                {
+                    monthData.revenueForTheCurrentMonth = CalculateThePercentage
+                        (_depositAmount, (decimal)monthData.percent, _daysEndOfTheMonth);
+
+                    _depositAmount += monthData.revenueForTheCurrentMonth;
+
+                    _daysEndOfTheMonth = DateTime.DaysInMonth(_dateTime.Year, _dateTime.Month);
+                    monthData.lastDayOfMonth = _dateTime.AddDays(DaysEndOfTheMonth(_dateTime)+1);
+                    
+                    _dateTime = monthData.lastDayOfMonth;
+                    ArrayMonthData.Add(monthData);
+                }
+            }
+            return ArrayMonthData;
+        }
+
+
+        private int DaysEndOfTheMonth(in DateTime dateTime) 
+        {
+            return DateTime.DaysInMonth(dateTime.Year, dateTime.Month) - dateTime.Day;
+        }
+
+        private decimal CalculateThePercentage(in decimal depositAmount, in decimal percent, in int daysEndOfTheMonth)
+        {
+            return (depositAmount * percent * daysEndOfTheMonth) / (365 * 100);
+        }
+
+        readonly private List<MonthData> ArrayMonthData = new List<MonthData>();
+    }
+}
---
+++ HomeworkTMS/HomeworkTMS/Program.cs
@@ -0,0 +1,64 @@
+﻿using HomeworkTMS;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+Bank bank = new Bank();
+var parrBankDep = bank.calculateContribution(18, 12.1, 1000).ToArray();
+
+Console.WriteLine("+-----------+-----------+--------+--------------+------------+");
+
+for (int i = 0; i < parrBankDep.Length; i++)
+{
+    var fd = parrBankDep[i].firstDayOfMonth.Date.ToString();
+    var ld = parrBankDep[i].lastDayOfMonth.Date.ToString();
+
+    Console.Write($"| {fd.Substring(3).Substring(0, fd.Length - 10)}");
+    Console.SetCursorPosition(12, i + 1);
+
+    Console.Write($"| {ld.Substring(3).Substring(0, fd.Length - 10)}");
+    Console.SetCursorPosition(24, i + 1);
+
+    Console.Write($"| {parrBankDep[i].percent} ");
+    Console.SetCursorPosition(33, i + 1);
+
+    Console.Write($"| {Math.Round(parrBankDep[i].depositAmount, 2)} BUN");
+    Console.SetCursorPosition(48, i + 1);
+
+    Console.Write($"| {Math.Round(parrBankDep[i].revenueForTheCurrentMonth, 2)} BYN");
+    Console.SetCursorPosition(60, i + 1);
+    Console.Write(" |");
+    Console.WriteLine();
+}
+Console.WriteLine("+-----------+-----------+--------+--------------+------------+");
+Console.WriteLine();
+Console.WriteLine($"Итого: {Math.Round(parrBankDep.Last().revenueForTheCurrentMonth + parrBankDep.Last().depositAmount, 2)}");
+
+//int[] a = { 1, 8, 1, 6 };
+//var b = BubbleSort(a);
+
+//foreach (var x in b)
+//    Console.WriteLine(x);
+
+//static int[] BubbleSort(int[] array)
+//{
+//    var len = array.Length;
+//    for (var i = 1; i < len; i++)
+//    {
+//        for (var j = 0; j < len - i; j++)
+//        {
+//            if (array[j] > array[j + 1])
+//            {
+//                Swap(ref array[j], ref array[j + 1]);
+//            }
+//        }
+//    }
+
+//    return array;
+//}
+//static void Swap(ref int e1, ref int e2)
+//{
+//    var temp = e1;
+//    e1 = e2;
+//    e2 = temp;
+//}
