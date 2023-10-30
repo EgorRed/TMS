4c8a440c17633c0bcc0f56e619150e12bd992059
EgorRed <egor.red02@gmail.com>
2023-10-30 19:33:56 +0300

    add chars

--- TMS_HttpGet/TMS_HttpGet/Program.cs
+++ TMS_HttpGet/TMS_HttpGet/Program.cs
@@ -1,35 +1,49 @@
-﻿var client = new HttpClient();
+﻿using System.Xml.Linq;
+
 var url = "https://ru.wikipedia.org/wiki/";
+var client = new HttpClient();
 
-for (var i = 'a'; i <= 'z'; i++)
+var chars = new char[]{'a', 'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+for (var i = 0; i <= chars.Length; i++)
 {
-    for (var j = 'a'; j <= 'z'; j++)
+    for (var j = 0; j <= chars.Length; j++)
     {
-        var path = url + "." + i + j;
-
-        HttpResponseMessage responce = await client.GetAsync(path);
-
-        if (responce.IsSuccessStatusCode)
+        string name = chars[i].ToString() + chars[j].ToString();
+        var path = url + "." + name;
+        
+        try
         {
-            Console.Write($"{url}{i}{j}");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("\t success");
-            Console.ForegroundColor = ConsoleColor.White;
+            HttpResponseMessage responce = await client.GetAsync(path);
+
+            if (responce.IsSuccessStatusCode)
+            {
+                Console.Write($"{url}{name}");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\t success");
+                Console.ForegroundColor = ConsoleColor.White;
 
 
-            var text = await responce.Content.ReadAsStringAsync();
-            var dataPath = $"..\\..\\..\\HtmlFiles\\{i}{j}.html";
+                var text = await responce.Content.ReadAsStringAsync();
+                var dataPath = $"..\\..\\..\\HtmlFiles\\{name}.html";
 
-            File.Create(dataPath).Close();
-            File.WriteAllText(dataPath, text);
+                File.Create(dataPath).Close();
+                File.WriteAllText(dataPath, text);
+            }
+            else
+            {
+                Console.Write($"{url}{name}");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\t not a success");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
         }
-        else
+        catch (Exception ex)
         {
-            Console.Write($"{url}{i}{j}");
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("\t not a success");
-            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine($"Ошибка при обработке страницы \"{path}\".\n{ex.Data}");
         }
+
 
     }
 }
