dac33eaeb4bd809cd9617f4447366366bbb1befa
EgorRed <egor.red02@gmail.com>
2023-11-30 18:12:31 +0300

    update

--- Registration/Registration/Program.cs
+++ Registration/Registration/Program.cs
@@ -5,11 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<UserDbContext>();
-builder.Services.AddSingleton<IUserManagerService, UserManagerService>();
+builder.Services.AddScoped<IUserManagerService, UserManagerService>();
 
 var app = builder.Build();
 
